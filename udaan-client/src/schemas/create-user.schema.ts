ce14ca47ab7ed0df4d2971c736e7e5123c82b3df
kale sigma <sigmastudent@gmail.com>
2025-11-25 23:55:01 +0530

    refining frontend schema validations

--- udaan-client/src/schemas/create-user.schema.ts
+++ udaan-client/src/schemas/create-user.schema.ts
@@ -1,6 +1,8 @@
 import { z } from "zod";
 
 // ✅ Regex patterns
+const nameRegex = /^[A-Za-z\s]+$/;
+const usernameRegex = /^[A-Za-z0-9]+$/;
 const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
 const strongPasswordRegex =
   /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&#^])[A-Za-z\d@$!%*?&#^]{8,}$/;
@@ -8,19 +10,29 @@ const strongPasswordRegex =
 export const createUserSchema = z.object({
   name: z
     .string()
+    .trim()
     .min(2, "Name must be at least 2 characters")
-    .max(50, "Name too long"),
+    .max(50, "Name too long")
+    .regex(nameRegex, "Name can contain only letters and spaces!"),
+
   username: z
     .string()
-    .min(3, "Username must be at least 3 characters")
-    .max(20, "Username too long"),
+    .trim()
+    .min(3, "Username must be at least 3 characters!")
+    .max(20, "Username too long")
+    .regex(usernameRegex, "Username can contain only letters and numbers!"),
   email: z
     .string()
-    .regex(emailRegex, "Enter a valid email address"),
+    .trim()
+    .toLowerCase()
+    .min(5, "Email is too short!")
+    .max(100, "Email is too long!")
+    .regex(emailRegex, "Enter a valid email address."),
   password: z
     .string()
-    .min(8, "Password must be at least 8 characters")
-    .max(50, "Password too long")
+    .trim()
+    .min(8, "Password must be at least 8 characters.")
+    .max(50, "Password too long!")
     .regex(
       strongPasswordRegex,
       "Password must contain uppercase, lowercase, number, and symbol"
@@ -28,7 +40,8 @@ export const createUserSchema = z.object({
   age: z
     .string()
     .min(1, "Age required")
-    .refine((val) => Number(val) >= 13, "Minimum age is 13"),
+    .refine((val) => Number(val) >= 13, { message: "Minimum age is 13!" })
+    .refine((val)=> Number(val) <=120, { message: "Please enter a valid age!" }),
 });
 
 export type CreateUserSchema = z.infer<typeof createUserSchema>;
